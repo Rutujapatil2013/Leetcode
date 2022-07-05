8392389b0aa1c5106c94b604378fc165f72e6fbf
Rutujapatil2013 <75259034+Rutujapatil2013@users.noreply.github.com>
2022-07-06 00:03:26 +0530

    Update 20-valid-parentheses.py

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -35,5 +35,32 @@ class Solution:
     
     #######################################
     
+    C++
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.length();
+        stack<char> ss;
+        for(int i = 0; i < n; i++) {
+            if(ss.empty()) {
+                if(s[i]=='(' || s[i]=='{' || s[i]=='[') ss.push(s[i]);
+                else return false;
+            }
+            else if(s[i]=='(' || s[i]=='{' || s[i]=='[') ss.push(s[i]);
+            else {
+                if(ss.top()=='(' && s[i]==')') ss.pop();
+                else if(ss.top()=='[' && s[i]==']') ss.pop();
+                else if(ss.top()=='{' && s[i]=='}') ss.pop();
+                else return false;
+            }
+        }
+        if(ss.empty()) return true;
+        return false;
+    }
+};
+
+###########################################################################
+    
     
     
