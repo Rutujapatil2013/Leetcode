4f5d2c1c882336941584c220739150ebc8a998b0
Rutujapatil2013 <75259034+Rutujapatil2013@users.noreply.github.com>
2022-07-06 00:01:53 +0530

    Time: 52 ms (38.36%), Space: 14.1 MB (24.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,10 +6,34 @@ class Solution:
             if char in d.values():
                 stack.append(char)
             else: 
-                # char in d.keys():
                 if stack==[] or (d[char]!=stack.pop()):
                     return False
-            # else:
-            #     return False
         return stack==[]
+    
+    
+##############################################################
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)%2!=0:
+            return False
+        matches = set([('(',')'),('{','}'),('[',']')])
+        opening = set('{([')
+        lst = []
+        
+        for i in s:
+            if i in opening:
+                lst.append(i)
+            else:
+                if len(lst)==0:
+                    return False
+                last_element = lst.pop()
+                if (last_element,i) not in matches:
+                    return False
+        return len(lst)==0
+    
+    #######################################
+    
+    
     
